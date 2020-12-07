f4a35eb7df550a0e888e57dde0ccaac0e9c4816c
Aditya Vasantharao <hsfan612@gmail.com>
2020-12-06 22:46:06 -0500

    Finish re2

--- src/Unit 3 - Regex/regex_2/regex.py
+++ src/Unit 3 - Regex/regex_2/regex.py
@@ -2,16 +2,16 @@ import sys; args = sys.argv[1:]
 idx = int(args[0])-40
 
 myRegexLst = [
-    r"/^[x.o]+$/i",
+    r"/^[x.o]{64}$/i",
     r"/^[xo]*[.][xo]*$/i",
-    r"/^(x+o*)?[.]|[.](o*x+)?$/i", #add .* on both sides of | if not work
-    r"/^.(..)*$/",
+    r"/^(x+o*)?[.]|[.](o*x+)?$/i",
+    r"/^.(..)*$/s",
     r"/^(0|1[01])([01]{2})*$/",
-    r"/\w*(a[eiou]|e[aiou]|i[eaou]|o[aeiu]|u[aeio])\w*/",
+    r"/\w*(a[eiou]|e[aiou]|i[eaou]|o[aeiu]|u[aeio])\w*/i",
     r"/^(0+|10+|1+$)*$/",
-    r"/^[bc]*a?[bc]*$/",
-    r"/^[bc]*((a[bc]*){2})*[bc]*$/",
-    r"/^0$|^(2[02]*)?((1[02]*){2})*$/"
+    r"/^(a|(b|c)[bc]*a?)[bc]*$/",
+    r"/^(a[bc]*a|b|c)([bc]|(a[bc]*){2})*$/",
+    r"/^(2[02]*|(1[02]*){2})+$/"
     ]
 
 if idx < len(myRegexLst):
